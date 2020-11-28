981371a684c9fc2631d45fdbf769439829847284
MineGame159 <petulko08@gmail.com>
2020-11-28 12:29:34 +0100

    Init

---
+++ src/main/java/minegame159/meteoranarchy/MeteorAnarchy.java
@@ -0,0 +1,13 @@
+package minegame159.meteoranarchy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MeteorAnarchy extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
