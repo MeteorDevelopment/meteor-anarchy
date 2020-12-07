1f15561c51099c76423b9e783b582349f7ae57eb
MineGame159 <petulko08@gmail.com>
2020-12-07 16:21:11 +0100

    Improved AntiCheat a bit

--- src/main/java/minegame159/meteoranarchy/listeners/AntiCheatListener.java
+++ src/main/java/minegame159/meteoranarchy/listeners/AntiCheatListener.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerVelocityEvent;
 
 public class AntiCheatListener implements Listener {
@@ -19,7 +18,8 @@ public class AntiCheatListener implements Listener {
     private final Object2ObjectMap<Player, Location> lastValidSpeedPositions = new Object2ObjectOpenHashMap<>();
     private final Object2ObjectMap<Player, Location> lastOnGroundPositions = new Object2ObjectOpenHashMap<>();
     private final Object2IntMap<Player> inAirTicks = new Object2IntOpenHashMap<>();
-    private final Object2IntMap<Player> notFallingTicks = new Object2IntOpenHashMap<>();
+    private final Object2IntMap<Player> inAirTicks2 = new Object2IntOpenHashMap<>();
+    private final Object2IntMap<Player> fallingTicks = new Object2IntOpenHashMap<>();
     private final Object2DoubleMap<Player> lastVelocityY = new Object2DoubleOpenHashMap<>();
     private final Object2IntMap<Player> highYVelocityTicks = new Object2IntOpenHashMap<>();
     private final Object2IntMap<Player> highButLessYVelocityTicks = new Object2IntOpenHashMap<>();
@@ -27,6 +27,8 @@ public class AntiCheatListener implements Listener {
     private final Object2BooleanMap<Player> lastWasFalling = new Object2BooleanOpenHashMap<>();
     //private final Object2ObjectMap<Player, Location> lastValidPhasePositions = new Object2ObjectOpenHashMap<>();
     private final Object2BooleanMap<Player> lastWasInsideVehicle = new Object2BooleanOpenHashMap<>();
+    private final Object2ObjectMap<Player, Location> lastPositions = new Object2ObjectOpenHashMap<>();
+    private final Object2BooleanMap<Player> ignoreSpeedNextTick = new Object2BooleanOpenHashMap<>();
 
     private final Object2ObjectMap<Player, int[]> lastSecondPerTickPlayerMoveTimes = new Object2ObjectOpenHashMap<>();
     private int lastSecondTickI = 0;
@@ -46,12 +48,12 @@ public class AntiCheatListener implements Listener {
 
         int totalMoveEventsLastSecond = 0;
         for (int x : lastSecondTicks) totalMoveEventsLastSecond += x;
-        if (totalMoveEventsLastSecond > 25) event.setCancelled(true);
+        if (totalMoveEventsLastSecond > 25.0 * (20.0 / Bukkit.getTPS()[0])) event.setCancelled(true);
 
         Location from = event.getFrom();
         Location to = event.getTo();
 
-        if (!ignore(player)) {
+        if (!ignore(player) && !ignoreSpeedNextTick.getBoolean(player)) {
             double y = 0;
             double velY = to.getY() - from.getY();
             if (velY > 2) y = Math.pow(velY, 2);
@@ -102,7 +104,8 @@ public class AntiCheatListener implements Listener {
         ignoreTicks.removeInt(player);
         lastOnGroundPositions.remove(player);
         inAirTicks.removeInt(player);
-        notFallingTicks.removeInt(player);
+        inAirTicks2.removeInt(player);
+        fallingTicks.removeInt(player);
         lastVelocityY.removeDouble(player);
         highYVelocityTicks.removeInt(player);
         highButLessYVelocityTicks.removeInt(player);
@@ -111,6 +114,8 @@ public class AntiCheatListener implements Listener {
         //lastValidPhasePositions.remove(player);
         lastWasInsideVehicle.removeBoolean(player);
         lastSecondPerTickPlayerMoveTimes.remove(player);
+        lastPositions.remove(player);
+        ignoreSpeedNextTick.removeBoolean(player);
     }*/
 
     @EventHandler
@@ -127,7 +132,7 @@ public class AntiCheatListener implements Listener {
             int[] ticks = lastSecondPerTickPlayerMoveTimes.get(player);
             if (ticks != null) ticks[lastSecondTickI] = 0;
 
-            if (player.isDead() || player.getGameMode() != GameMode.SURVIVAL) continue;
+            if (player.isDead()) continue;
 
             boolean isInsideVehicle = player.isInsideVehicle();
             if (!lastWasInsideVehicle.getBoolean(player) && isInsideVehicle) ignoreTicks.put(player, 6);
@@ -139,41 +144,78 @@ public class AntiCheatListener implements Listener {
                 timesStoppedFalling.removeInt(player);
             }
 
-            if (ignore(player) || player.isGliding()) continue;
+            ignoreSpeedNextTick.removeBoolean(player);
+            if (ignore(player)) continue;
 
-            double velY = lastVelocityY.getOrDefault(player, 0.0);
-            int yVelTicks = highYVelocityTicks.getInt(player);
-            int lessYVelTicks = highButLessYVelocityTicks.getInt(player);
+            if (!player.isGliding()) {
+                // Flight
+                double velY = lastVelocityY.getOrDefault(player, 0.0);
+                int yVelTicks = highYVelocityTicks.getInt(player);
+                int lessYVelTicks = highButLessYVelocityTicks.getInt(player);
 
-            if (lastWasFalling.getBoolean(player) && velY >= 0) {
-                int timesStoppedFalling = this.timesStoppedFalling.getInt(player) + 1;
-                this.timesStoppedFalling.put(player, timesStoppedFalling);
+                if (lastWasFalling.getBoolean(player) && velY >= 0) {
+                    int timesStoppedFalling = this.timesStoppedFalling.getInt(player) + 1;
+                    this.timesStoppedFalling.put(player, timesStoppedFalling);
+                }
+
+                lastWasFalling.put(player, velY < 0);
+
+                int ticksFalling = 0;
+
+                if (!onGround && velY < 0) {
+                    ticksFalling = fallingTicks.getInt(player) + 1;
+                    fallingTicks.put(player, ticksFalling);
+                } else {
+                    fallingTicks.removeInt(player);
+                }
+
+                boolean inAir = !onGround && velY >= 0 && (velY <= 0.25 || yVelTicks > 4 || lessYVelTicks > 8);
+                int ticksInAir = 0;
+
+                if (inAir) {
+                    ticksInAir = inAirTicks.getInt(player) + 1;
+                    inAirTicks.put(player, ticksInAir);
+                } else {
+                    inAirTicks.removeInt(player);
+                }
+
+                boolean inAir2 = !onGround;
+                if (!onGround && velY < 0 && ticksFalling > 4) inAir2 = false;
+                int ticksInAir2 = 0;
+
+                if (inAir2) {
+                    ticksInAir2 = inAirTicks2.getInt(player) + 1;
+                    inAirTicks2.put(player, ticksInAir2);
+                } else {
+                    inAirTicks2.removeInt(player);
+                }
+
+                if (player.getGameMode() == GameMode.SURVIVAL && ((ticksInAir >= 10) || (ticksInAir2 > 14) || (inAir && timesStoppedFalling.getInt(player) > 1))) {
+                    Location pos = lastOnGroundPositions.get(player);
+                    if (pos != null) {
+                        player.teleport(pos);
+                        ignoreSpeedNextTick.put(player, true);
+                    }
+                }
             }
 
-            lastWasFalling.put(player, velY < 0);
+            // Tick Speed
+            /*Location pos = player.getLocation();
 
-            /*int notFallingTicks = 0;
-            if (velY >= 0) {
-                notFallingTicks = this.notFallingTicks.getInt(player) + 1;
-                this.notFallingTicks.put(player, notFallingTicks);
-            } else {
-                this.notFallingTicks.removeInt(player);
-            }*/
+            if (player.getGameMode() == GameMode.SURVIVAL && !ignoreSpeedNextTick.getBoolean(player)) {
+                Location lastPos = lastPositions.getOrDefault(player, pos);
+                double speed = Math.sqrt(Math.pow(pos.getX() - lastPos.getX(), 2) + Math.pow(pos.getZ() - lastPos.getZ(), 2));
 
-            boolean inAir = !onGround && velY >= 0 && (velY <= 0.25 || yVelTicks > 4 || lessYVelTicks > 8);
-            int ticksInAir = 0;
+                double maxSpeed = 2;
+                if (player.isGliding()) maxSpeed = 5;
 
-            if (inAir) {
-                ticksInAir = inAirTicks.getInt(player);
-                inAirTicks.put(player, ticksInAir + 1);
-            } else {
-                inAirTicks.removeInt(player);
+                if (speed > maxSpeed * (20.0 / Bukkit.getTPS()[0])) {
+                    pos = lastPos;
+                    player.teleport(pos);
+                }
             }
 
-            if ((ticksInAir >= 10)/* || (inAir && notFallingTicks >= 100)*/ || (inAir && timesStoppedFalling.getInt(player) > 1)) {
-                Location pos = lastOnGroundPositions.get(player);
-                if (pos != null) player.teleport(pos);
-            }
+            lastPositions.put(player, pos);*/
 
             // Phase
             /*if (isInBlock(player)) {
@@ -192,15 +234,28 @@ public class AntiCheatListener implements Listener {
     private boolean isOnGround(Player player) {
         Location pos = player.getLocation();
 
-        for (int y = 0; y >= -1; y--) {
-            for (int x = -1; x < 2; x++) {
-                for (int z = -1; z < 2; z++) {
-                    Block block = pos.add(x, y, z).getBlock();
+        if (check(pos, 0, 0)) return true;
 
-                    if (!block.isPassable() || isClimbable(block) || block.isLiquid()) return true;
-                    pos.subtract(x, y, z);
-                }
-            }
+        double xa = pos.getX() - pos.getBlockX();
+        double za = pos.getZ() - pos.getBlockZ();
+
+        if (xa >= 0 && xa <= 0.3 && check(pos, -1, 0)) return true;
+        if (xa >= 0.7 && check(pos, 1, 0)) return true;
+        if (za >= 0 && za <= 0.3 && check(pos, 0, -1)) return true;
+        if (za >= 0.7 && check(pos, 0, 1)) return true;
+
+        if (xa >= 0 && xa <= 0.3 && za >= 0 && za <= 0.3 && check(pos, -1, -1)) return true;
+        if (xa >= 0 && xa <= 0.3 && za >= 0.7 && check(pos, -1, 1)) return true;
+        if (xa >= 0.7 && za >= 0 && za <= 0.3 && check(pos, 1, -1)) return true;
+        return xa >= 0.7 && za >= 0.7 && check(pos, 1, 1);
+    }
+
+    private boolean check(Location pos, int x, int z) {
+        for (int y = 0; y >= -1; y--) {
+            Block block = pos.add(x, y, z).getBlock();
+
+            if (!block.isPassable() || isClimbable(block) || block.isLiquid()) return true;
+            pos.subtract(x, y, z);
         }
 
         return false;
